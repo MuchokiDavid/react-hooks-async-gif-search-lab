91abd9d909487f6e21cf44aa9484abb51aed9cae
dmmuchoki7 <dmmuchoki7@gmail.com>
2023-11-26 21:00:41 +0300

    Corrected fetch

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,3 @@
-import React, { useEffect, useState } from "react";
-
 import NavBar from "./NavBar";
 import GifListContainer from "./GifListContainer";
 
@@ -7,44 +5,10 @@ import GifListContainer from "./GifListContainer";
 
 function App() {
 
-  const apiKey= "KYeZAYMCZYbVBOz0rrNmgKGoSGoTpoOH";
-  const [gifs, setGifs]= useState([])
-  const [loading, setLoading]= useState(true)
-  const [error, setError]= useState(null)
-
-
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await fetch(`https://api.giphy.com/v1/gifs/search?q=dolphin&api_key=${apiKey}&rating=g`);
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-
-        const data = await response.json();
-        setGifs(data);
-      } catch (error) {
-        setError(error.message);
-      } finally {
-        setLoading(false);
-      }
-    }
-
-    fetchData();
-  }, []);
-
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-
-  if (error) {
-    return <p>Error: {error}</p>;
-  }
-
-  return (
+   return (
     <div>
       <NavBar color="black" title="Giphy Search" />
-      <GifListContainer gifs= {gifs}/>
+      <GifListContainer/>
     </div>
   );
 }
--- src/components/GifListContainer.js
+++ src/components/GifListContainer.js
@@ -1,9 +1,46 @@
 import React from 'react'
+import { useEffect, useState } from "react";
+import GifList from './GifList';
 
-function GifListContainer(gifs) {
-    console.log(gifs)
+function GifListContainer() { 
+const apiKey= "KYeZAYMCZYbVBOz0rrNmgKGoSGoTpoOH";
+  const [gifs, setGifs]= useState([])
+  const [loading, setLoading]= useState(true)
+  const [error, setError]= useState(null)
+
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(`https://api.giphy.com/v1/gifs/search?q=dolphin&api_key=${apiKey}&rating=g`);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+        setGifs(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
   return (
-    <div>GifListContainer</div>
+    <div>
+        <GifList/>
+    </div>
   )
 }
 
